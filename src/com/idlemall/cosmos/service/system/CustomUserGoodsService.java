2bece69798dbb1c7c7cd4188d988b77236c544a5
CosmosRay <raycosmos@qq.com>
2018-04-13 14:34:36 +0800

    service-system

---
+++ src/com/idlemall/cosmos/service/system/CustomUserGoodsService.java
@@ -0,0 +1,10 @@
+package com.idlemall.cosmos.service.system;
+
+
+/**用户商品管理Service
+ * @author CosmosRay
+ *
+ */
+public interface CustomUserGoodsService {
+
+}
---
+++ src/com/idlemall/cosmos/service/system/CustomUserLogService.java
@@ -0,0 +1,10 @@
+package com.idlemall.cosmos.service.system;
+
+
+/**用户登录Service
+ * @author CosmosRay
+ *
+ */
+public interface CustomUserLogService {
+
+}
